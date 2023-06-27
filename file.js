7a9982730c8eb903c7402fe4512d76d123d54017
mayuresh0508 <mayu.khanzode@gmail.com>
2023-06-28 03:11:58 +0530

    height navbar

--- file.js
+++ file.js
@@ -15,36 +15,32 @@ document.addEventListener("DOMContentLoaded", () => {
     const navbarlinks = document.getElementsByClassName("right-nav")[0];
     const navbar = document.getElementsByClassName("nav")[0];
     const mediaQuery = window.matchMedia("(max-width: 750px)");
-
+  
     const toggleNavbar = () => {
-        navbarlinks.classList.toggle("active");
-        
-        if (navbarlinks.classList.contains("active")) {
-            navbar.style.height = "160px"; 
-        } else {
-            navbar.style.height = "50px"; 
-        }
+      navbarlinks.classList.toggle("active");
+  
+      if (navbarlinks.classList.contains("active")) {
+        navbar.style.height = "165px"; 
+      } else {
+        navbar.style.height = mediaQuery.matches ? "50px" : "80px"; 
+      }
     };
-
+  
     const handleMediaQueryChange = (event) => {
-        if (event.matches) {
-        
-            navbar.style.height = "50px"; 
-            hamburger.addEventListener("click", toggleNavbar);
-
-            const listItems = navbarlinks.getElementsByTagName("li");
-            for (let i = 0; i < listItems.length; i++) {
-                listItems[i].addEventListener("click", toggleNavbar);
-            }
-        } else {
-            hamburger.removeEventListener("click", toggleNavbar);
-            navbarlinks.classList.remove("active");
-            navbar.style.height = "50px"; 
-        }
+      if (event.matches) {
+    
+        navbar.style.height = "50px"; 
+        hamburger.addEventListener("click", toggleNavbar);
+      } else {
+        hamburger.removeEventListener("click", toggleNavbar);
+        navbarlinks.classList.remove("active");
+        navbar.style.height = "80px"; 
+      }
     };
-
+  
     handleMediaQueryChange(mediaQuery); 
-
+  
     mediaQuery.addEventListener("change", handleMediaQueryChange); 
 });
+  
 
