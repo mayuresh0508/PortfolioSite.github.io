b8771ad0b2c07aefefb38d2ee2c5dd458efa8be0
mayuresh0508 <mayu.khanzode@gmail.com>
2023-06-28 13:24:20 +0530

    colapse

--- file.js
+++ file.js
@@ -1,46 +1,61 @@
 const blob = document.getElementById("blob");
 
 window.onpointermove = event => {
-    const { clientX, clientY } = event;
+  const { clientX, clientY } = event;
 
-    blob.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-    }, { duration: 3000, fill: "forwards" });
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementsByClassName("hamburger")[0];
-    const navbarlinks = document.getElementsByClassName("right-nav")[0];
-    const navbar = document.getElementsByClassName("nav")[0];
-    const mediaQuery = window.matchMedia("(max-width: 750px)");
-  
-    const toggleNavbar = () => {
-      navbarlinks.classList.toggle("active");
-  
-      if (navbarlinks.classList.contains("active")) {
-        navbar.style.height = "165px"; 
-      } else {
-        navbar.style.height = mediaQuery.matches ? "50px" : "80px"; 
+  const hamburger = document.getElementsByClassName("hamburger")[0];
+  const navbarlinks = document.getElementsByClassName("right-nav")[0];
+  const navbar = document.getElementsByClassName("nav")[0];
+  const mediaQuery = window.matchMedia("(max-width: 750px)");
+
+  const toggleNavbar = () => {
+    navbarlinks.classList.toggle("active");
+
+    if (navbarlinks.classList.contains("active")) {
+      navbar.style.height = "165px";
+    } else {
+      navbar.style.height = mediaQuery.matches ? "50px" : "80px";
+    }
+  };
+
+  const handleMediaQueryChange = (event) => {
+    if (event.matches) {
+
+      navbar.style.height = "50px";
+      hamburger.addEventListener("click", toggleNavbar);
+
+      const listItems = navbarlinks.getElementsByTagName("a");
+      for (let i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener("click", () => {
+          navbarlinks.classList.remove("active");
+          navbar.style.height = "50px";
+        });
       }
-    };
-  
-    const handleMediaQueryChange = (event) => {
-      if (event.matches) {
-    
-        navbar.style.height = "50px"; 
-        hamburger.addEventListener("click", toggleNavbar);
-      } else {
-        hamburger.removeEventListener("click", toggleNavbar);
-        navbarlinks.classList.remove("active");
-        navbar.style.height = "80px"; 
+    } else {
+      hamburger.removeEventListener("click", toggleNavbar);
+      navbarlinks.classList.remove("active");
+      navbar.style.height = "80px";
+
+      const listItems = navbarlinks.getElementsByTagName("a");
+      for (let i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener("click", () => {
+          navbar.style.height = "80px"; // Set the height for desktop
+        });
       }
-    };
-  
-    handleMediaQueryChange(mediaQuery); 
-  
-    mediaQuery.addEventListener("change", handleMediaQueryChange); 
+    }
+  };
+
+  handleMediaQueryChange(mediaQuery);
+
+  mediaQuery.addEventListener("change", handleMediaQueryChange);
 });
-  
+
 
